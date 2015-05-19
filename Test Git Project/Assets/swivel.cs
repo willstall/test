ae5dba181fc44877a824a07b1d68e22e832047a8
willstall <will@cipherprime.com>
2015-05-19 16:33:53 -0400

    Scene Update

---
+++ Test Git Project/Assets/swivel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class swivel : MonoBehaviour {
+
+	public float amount;
+	private float pos;
+	private Vector3 initialPos;
+	// Use this for initialization
+	void Start () {
+		pos = amount;
+		initialPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = initialPos + Vector3.up * amount * Mathf.Sin( Time.time );
+	}
+}
